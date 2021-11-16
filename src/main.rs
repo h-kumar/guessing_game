8212f766bdd06f31b4329e48b0f9af445aeca833
h-kumar <k.harish@outlook.com>
2021-11-16 22:47:29 +0530

    first guessing game

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Pleasen type a number!");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
         println!("You guessed: {}", guess);
 //compare the input against secret number
